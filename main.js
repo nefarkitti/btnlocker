f61b6f85a096cf2ab7350d1a275b4ea03a4e6cca
Kenzi <89108711+nefarkitti@users.noreply.github.com>
2026-08-08 10:22:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function copy(id) {
 
-    navigator.clipboard.writeText(`https://nefarkitti.github.io/btnlocker/btns/${id}.gif`)
+    navigator.clipboard.writeText(`<a href=""><img src="https://nefarkitti.github.io/btnlocker/btns/${id}.gif"></a>`)
 
 }
